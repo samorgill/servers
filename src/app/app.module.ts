1ad1e6baff280faef7e980a1c4136866fca68655
samorgill <orgillsams@gmail.com>
2018-03-31 17:26:37 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { GameControlComponent } from './game-control/game-control.component';
 import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { OddComponent } from './odd/odd.component';
     CockpitComponent,
     ServerElementComponent,
     GameControlComponent,
-    OddComponent
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
